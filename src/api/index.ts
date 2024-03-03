78041f63ecdb3bb4b9b0e938c288389dbebc3c70
leomosley <leo.mosley2@gmail.com>
2024-03-03 22:52:20 +0000

    added frames, updated api structure, added redirect if not curl request, working frames animation on local machine

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,100 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { promises as fs, Dirent } from 'fs';
+import * as path from 'path';
+import { Readable } from 'stream';
 
-import MessageResponse from '../interfaces/MessageResponse';
-import test from './test-route';
+interface StreamerOptions {
+  flip: boolean;
+}
 
-const router = express.Router();
+let frames: string[] = [];
 
-router.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({message: 'api index'});
+async function loadFrames(framesPath: string): Promise<void> {
+  try {
+    const files: Dirent[] = await fs.readdir(framesPath, { withFileTypes: true });
+
+    frames = [];
+
+    for (const file of files) {
+      const filePath = path.join(framesPath, file.name);
+      const stat = await fs.stat(filePath);
+
+      if (stat.isFile()) {
+        const frame = await fs.readFile(filePath);
+        frames.push(frame.toString());
+      }
+    }
+  } catch (error) {
+    console.error(`Error loading frames: ${error}`);
+  }
+}
+
+function streamer(stream: Readable, opts: StreamerOptions): NodeJS.Timer {
+  let index = 0;
+
+  return setInterval(() => {
+    let char = opts.flip ? frames[index].split('').reverse().join('') : frames[index];
+    stream.push('\x1b[2J\x1b[3J\x1b[H');
+    stream.push(char);
+
+    index = (index + 1) % frames.length;
+  }, 70);
+}
+
+const validateQuery = ({ flip }: { flip?: string }) => ({
+  flip: !!flip,
 });
 
-router.use('/test', test);
+const router = express.Router()
+
+router.get('/:path', async (req: Request, res: Response) => {
+  const requestedPath: string = req.params.path;
+  const framesBasePath: string = path.join(__dirname, '../frames/');
+  const framesPath: string = framesBasePath + requestedPath
+
+  if (requestedPath === 'healthcheck') {
+    res.status(200).json({ status: 'ok' });
+    return;
+  }
+
+  if (
+    req.headers &&
+    req.headers['user-agent'] &&
+    !req.headers['user-agent'].includes('curl')
+  ) {
+    res.writeHead(302, { Location: 'https://github.com/leomosley/art' });
+    return res.end();
+  }
+
+  try {
+    const framesDirectoryExists = await fs.access(framesPath)
+      .then(() => true)
+      .catch(() => false);
+    
+    if (
+      !requestedPath || 
+      requestedPath === '/' || 
+      !framesDirectoryExists
+    ) {
+      return res.status(404).send('Frames directory not found');
+    }
+    
+    await loadFrames(framesPath);
+  } catch (err) {
+    res.status(500);
+    return;
+  }
+
+  const stream = new Readable();
+  stream._read = function noop() {};
+  stream.pipe(res);
+
+  const interval: NodeJS.Timer = streamer(stream, validateQuery(req.query as { flip?: string }));
+
+  req.on('close', () => {
+    stream.destroy();
+    clearInterval(interval as unknown as number);
+  });
+});
 
 export default router;
--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,8 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
 
-import * as middlewares from './middlewares';
+import { notFound, errorHandler } from './middlewares';
 import api from './api';
-import MessageResponse from './interfaces/MessageResponse';
 
 require('dotenv').config();
 
@@ -16,13 +15,9 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({message: 'app'});
-});
+app.use('/', api);
 
-app.use('/api/v1', api);
-
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
